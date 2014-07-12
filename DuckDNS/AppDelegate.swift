559138139cded6f7738ce2b632a48ba418276d06
Jeff Hanbury <jeff@marmaladesoul.com>
2014-07-12 16:43:44 +1200

    Initial Commit

---
+++ DuckDNS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DuckDNS
+//
+//  Created by Jeff Hanbury on 12/07/2014.
+//  Copyright (c) 2014 Marmaladesoul. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DuckDNS/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  DuckDNS
+//
+//  Created by Jeff Hanbury on 12/07/2014.
+//  Copyright (c) 2014 Marmaladesoul. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
