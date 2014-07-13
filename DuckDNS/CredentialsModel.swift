1c138d91d15f165a9145176d7d2b759d913d3353
Jeff Hanbury <jeff@marmaladesoul.com>
2014-07-13 16:27:27 +1200

    Update AXStatusItemPopup submodjule.

---
+++ DuckDNS/CredentialsModel.swift
@@ -0,0 +1,13 @@
+//
+//  CredentialsModel.swift
+//  DuckDNS
+//
+//  Created by Jeff Hanbury on 13/07/2014.
+//  Copyright (c) 2014 Marmaladesoul. All rights reserved.
+//
+
+import Foundation
+
+class CredentialsModel {
+    
+}
